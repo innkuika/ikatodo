ddd9c6c12486dbfb0ce0f971b181523164935999
innkuika <innkuika@gmail.com>
2020-07-06 17:59:50 -0400

    office hour model(?

---
+++ officehour_models.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+class OfficeHour(object):
+    def __init__(self, course_id: str, host: str, role: str, location: str, day: str, time_begin: int, duration: int):
+        self.course_id = course_id
+        self.host = host
+        self.role = role
+        self.location = location
+        self.day = day
+        self.time_begin = time_begin
+        self.duration = duration
+
+    def to_json(self):
+        return{
+            "Coures ID": self.course_id,
+            "Host": self.host,
+            "Location": self.location,
+            "Day": self.day,
+            "Time Begin": str(self.time_begin),
+            "Role": self.role,
+            "Duration": str(self.duration)
+        }
